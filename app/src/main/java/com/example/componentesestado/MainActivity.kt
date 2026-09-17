e501cafd4c261440f7d35deeb63718f68b4090de
Anderson Ribeiro <anderson.soares.4245@gmail.com>
2026-09-17 11:27:39 -0300

    enviando arquivos

---
+++ app/src/main/java/com/example/componentesestado/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.componentesestado
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.componentesestado.ui.theme.ComponentesEstadoTheme
+import com.example.componentesestado.ui.theme.Righteous
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComponentesEstadoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicComponentsScreen(
+                        modifier = Modifier.padding(innerPadding))
+//                    AndroidEnemy(
+//                        color = Color.Red,
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicComponentsScreen( modifier: Modifier = Modifier) {
+
+    val gardientBrush = Brush.verticalGradient(
+        colors = listOf(
+            Color(239, 247, 207), // Cor inicial (bege)
+            Color(255, 255, 255)  // Cor final (Branco)
+        )
+    )
+
+    Column(
+        modifier
+            .fillMaxSize()
+//            .background(Color(239, 247, 207))
+            .background(brush = gardientBrush)
+    ) {
+        Text(
+            text = "Aulas android",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF3DDC84),
+            fontFamily = FontFamily.Serif,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.End,
+            letterSpacing = 4.sp
+        )
+        Text(
+            text = "com Jetpack Compose",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color(66, 133, 244),
+            fontFamily = Righteous,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+
+@Composable
+fun AndroidEnemy(
+    color: Color,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.ic_launcher_foreground),
+        colorFilter = ColorFilter.tint(color = color),
+        contentDescription = "Android Enemy"
+    )
+}
