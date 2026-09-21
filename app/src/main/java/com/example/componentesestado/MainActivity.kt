adbcfdc91472858ff137f966db03c66416c4b895
Anderson Ribeiro <anderson.soares.4245@gmail.com>
2026-09-21 11:22:42 -0300

    mudando estado de componentes

--- app/src/main/java/com/example/componentesestado/MainActivity.kt
+++ app/src/main/java/com/example/componentesestado/MainActivity.kt
@@ -4,16 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -22,8 +46,11 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.componentesestado.ui.theme.ComponentesEstadoTheme
 import com.example.componentesestado.ui.theme.Righteous
@@ -57,11 +84,48 @@ fun BasicComponentsScreen( modifier: Modifier = Modifier) {
         )
     )
 
+    var textFieldValue by remember {
+        mutableStateOf("")
+    }
+
+    val quantidade = remember {
+        mutableStateOf("")
+    }
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var lembrar by remember {
+        mutableStateOf(false)
+    }
+
+    var kotlin by remember {
+        mutableStateOf(false)
+    }
+
+    var jetCompose by remember {
+        mutableStateOf(false)
+    }
+
+    var android by remember {
+        mutableStateOf(false)
+    }
+
+    var sistemaSelecionado by remember {
+        mutableStateOf(0)
+    }
+
+    var corFundo by remember {
+        mutableStateOf(Color(239, 247, 207))
+    }
+
     Column(
         modifier
             .fillMaxSize()
 //            .background(Color(239, 247, 207))
-            .background(brush = gardientBrush)
+//            .background(brush = gardientBrush)
+            .background(color = corFundo),
     ) {
         Text(
             text = "Aulas android",
@@ -83,10 +147,200 @@ fun BasicComponentsScreen( modifier: Modifier = Modifier) {
         )
 
         TextField(
-            value = "",
-            onValueChange = {},
-            modifier = Modifier.fillMaxWidth()
+            value = textFieldValue,
+            onValueChange = { novoValor ->
+                textFieldValue = novoValor
+            },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words),
+            label = {
+                Text(text = "Nome e sobrenome")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Ícone pessoa",
+//                    tint = Color(0xFF3DDC84)
+                    tint = Color(66, 133, 244)
+                )
+            }
         )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        TextField(
+            value = quantidade.value,
+            onValueChange = { novoValor ->
+                quantidade.value = novoValor
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            placeholder = {
+                Text(text = "Qual a quantidade?")
+            },
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = Color.Blue,
+                unfocusedTextColor = Color.Red,
+                unfocusedPlaceholderColor = Color.Magenta
+            ),
+            singleLine = true  // Limita a quantidade de linhas para apenas uma linha e bloqueia o enter
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it},
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Email")
+            },
+            placeholder = {
+                Text(text = "Digite seu email")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Ícone pessoa",
+                    tint = Color(66, 133, 244)
+                )
+            },
+            shape = RoundedCornerShape(
+                topStart = 32.dp,
+//                topEnd = 32.dp,
+//                bottomStart = 32.dp,
+                bottomEnd = 32.dp
+            ),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Blue,
+                focusedLabelColor = Color.Blue,
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White
+            )
+        )
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                checked = lembrar,
+                onCheckedChange = { lembrar = it },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.Blue,
+                    uncheckedColor = Color.Red
+                )
+            )
+
+            Text(text = "Lembrar de mim")
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                checked = kotlin,
+                onCheckedChange = { kotlin = it },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.Blue,
+                    uncheckedColor = Color.Red
+                )
+            )
+
+            Text(text = "Kotlin")
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                checked = jetCompose,
+                onCheckedChange = { jetCompose = it },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.Blue,
+                    uncheckedColor = Color.Red
+                )
+            )
+
+            Text(text = "Jetpack Compose")
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                checked = android,
+                onCheckedChange = { android = it },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.Blue,
+                    uncheckedColor = Color.Red
+                )
+            )
+
+            Text(text = "Android")
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = sistemaSelecionado == 1,
+                onClick = { sistemaSelecionado = 1 }
+            )
+
+            Text(text = "MacOs")
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = sistemaSelecionado == 2,
+                onClick = { sistemaSelecionado = 2 }
+            )
+
+            Text(text = "Windows")
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = sistemaSelecionado == 3,
+                onClick = { sistemaSelecionado = 3 }
+            )
+
+            Text(text = "Linux")
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(
+                onClick = {
+                    corFundo = Color.Red
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue,
+                    contentColor = Color.Green
+                ),
+                border = BorderStroke(4.dp, Color.Red),
+                shape = RoundedCornerShape(topEnd = 12.dp, bottomStart = 12.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Star, contentDescription = "Star")
+                Text(text = "Clique aqui!")
+            }
+
+            OutlinedButton(
+                onClick = {
+                    corFundo = Color.Cyan
+                }
+            ) {
+                Text(text = "Outro botão")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            OutlinedButton(
+                onClick = {
+                    corFundo = Color(239, 247, 207)
+                }
+            ) {
+                Text(text = "Voltar fundo")
+            }
+        }
+
     }
 }
 
